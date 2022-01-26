2269947a63e50c4fca8aef26173bd3c6cac351c3
Mato <carpenters.algorithm@gmail.com>
2022-01-27 00:06:47 +0900

    #0005 View.Transactors.ConnectionStateObserver作成

---
+++ Assets/Scripts/Sources/View/Transactors/ConnectionStateObserver.cs
@@ -0,0 +1,36 @@
+using UniRx;
+using Zenject;
+using MatoApp.Eleven.Network;
+using MatoApp.Utilities;
+
+namespace MatoApp.Eleven.View
+{
+    /// <summary>
+    /// Connectionが確立されたらLobbySceneに遷移するクラス
+    /// </summary>
+    internal class ConnectionStateObserver : IInitializable
+    {
+        public IConnectionStatePublisher ConnectionStatePublisher { private get; init; }
+        public ISceneLoader SceneLoader { private get; init; }
+
+        public ConnectionStateObserver() { }
+        [Inject]
+        public ConnectionStateObserver(
+            [Inject] IConnectionStatePublisher connectionStatePublisher,
+            [Inject] ISceneLoader sceneLoader)
+        {
+            ConnectionStatePublisher = connectionStatePublisher;
+            SceneLoader = sceneLoader;
+        }
+
+        public void Initialize()
+        {
+            ConnectionStatePublisher
+                .OnConnected
+                .Subscribe(async _ =>
+                {
+                    await SceneLoader.LoadSceneAsync(Scene.Lobby);
+                });
+        }
+    }
+}
---
+++ Assets/Scripts/Tests/View/Transactors/ConnectionStateObserverTest.cs
@@ -0,0 +1,48 @@
+using NUnit.Framework;
+using NSubstitute;
+using UniRx;
+using MatoApp.Eleven.Network;
+using MatoApp.Utilities;
+using Assert = UnityEngine.Assertions.Assert;
+
+namespace MatoApp.Eleven.View.Tests
+{
+    internal class ConnectionStateObserverTest
+    {
+        private ConnectionStateObserver ConnectionStateObserver { get; set; }
+
+        private IConnectionStatePublisher ConnectionStatePublisher { get; set; }
+        private ISceneLoader SceneLoader { get; set; }
+
+        private Subject<Unit> OnConnectedSubject { get; set; }
+
+        [OneTimeSetUp]
+        public void Initialize() { }
+
+        [SetUp]
+        public void SetUp()
+        {
+            ConnectionStatePublisher = Substitute.For<IConnectionStatePublisher>();
+            SceneLoader = Substitute.For<ISceneLoader>();
+
+            OnConnectedSubject = new();
+            ConnectionStatePublisher.OnConnected.Returns(OnConnectedSubject.AsObservable());
+
+            ConnectionStateObserver = new()
+            {
+                ConnectionStatePublisher = ConnectionStatePublisher,
+                SceneLoader = SceneLoader
+            };
+        }
+
+        [Test]
+        public void _01_Connectionが確立された()
+        {
+            ConnectionStateObserver.Initialize();
+
+            OnConnectedSubject.OnNext(default);
+
+            SceneLoader.Received().LoadSceneAsync(Scene.Lobby);
+        }
+    }
+}
