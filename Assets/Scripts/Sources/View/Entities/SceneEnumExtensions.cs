0bd85dba24619c92ad353b565d558175ea345250
Mato <carpenters.algorithm@gmail.com>
2022-01-30 20:13:48 +0900

    SceneEnumの機能拡張クラスを作成

--- Assets/Scripts/Sources/View/Entities/SceneEnumExtensions.cs
+++ Assets/Scripts/Sources/View/Entities/SceneEnumExtensions.cs
@@ -1,5 +1,8 @@
 namespace MatoApp.Eleven.View
 {
+    /// <summary>
+    /// SceneEnumの機能拡張クラス
+    /// </summary>
     public static class SceneEnumExtensions 
     {
         public static string GetName(this Scene scene)
