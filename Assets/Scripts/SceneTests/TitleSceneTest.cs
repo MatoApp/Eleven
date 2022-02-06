1a07d212888395765eb17b31f144c30b53282057
Mato <carpenters.algorithm@gmail.com>
2022-02-06 15:45:09 +0900

    TitleSceneTestを修正

--- Assets/Scripts/SceneTests/TitleSceneTest.cs
+++ Assets/Scripts/SceneTests/TitleSceneTest.cs
@@ -7,6 +7,7 @@ using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.TestTools;
 using TMPro;
+using Photon.Pun;
 using MatoApp.Eleven.View;
 using MatoApp.Utilities;
 using Assert = UnityEngine.Assertions.Assert;
@@ -20,7 +21,7 @@ namespace MatoApp.Eleven.Tests
         private ISceneLoader SceneLoader { get; set; }
 
         [UnityTest]
-        public IEnumerator _01_Usernameを入力してSigninButtonを押すとLobbySceneに遷移する() => UniTask.ToCoroutine(async () =>
+        public IEnumerator _01_Usernameを入力してSigninButtonを押すとUsernameをNetworkに反映した後LobbySceneに遷移する() => UniTask.ToCoroutine(async () =>
         {
             await LoadScene(Scene.Title.GetName());
 
@@ -37,17 +38,17 @@ namespace MatoApp.Eleven.Tests
             Assert.IsNotNull(UsernameInputField);
             Assert.IsNotNull(SceneLoader);
 
+            PhotonNetwork.NickName = "";
+            Assert.AreNotEqual("username", PhotonNetwork.NickName);
+
             UsernameInputField.text = "username";
             SigninButton.onClick.Invoke();
 
             var cts = new CancellationTokenSource();
             cts.CancelAfterSlim(TimeSpan.FromSeconds(5));
-
-            SceneLoader.LoadScene(Scene.Lobby);
-
             await SceneLoader.OnSceneLoaded.ToUniTask(useFirstValue: true, cancellationToken: cts.Token);
 
-            Assert.AreEqual(true, true);
+            Assert.AreEqual("username", PhotonNetwork.NickName);
         });
     }
 }
